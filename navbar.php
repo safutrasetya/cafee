b2074598e62da1b93b4960bfa755cab8902487c6
safutrasetya <safutrasetya@gmail.com>
2021-10-12 19:05:23 +0700

    Merge branch 'master' of https://github.com/safutrasetya/cafee

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <img src="/flexbox/img/img1.png" alt="" width="30" height="24">
+    <a class="navbar-brand" href="#" style ="margin-left: 5px">About Us</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      
+    </div>
+  </div>
+    <form class="d-flex">
+        <img src="/flexbox/img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px">
+        <img src="/flexbox/img/img1.png" alt="" width="30" height="24" style ="margin-right: 15px">
+    </form>
+</nav>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+  </body>
+</html>
