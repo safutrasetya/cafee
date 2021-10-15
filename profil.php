55e113a3d23795fc553250473243296890e394e7
immanuel1210 <immanuelsimarsoit@gmail.com>
2021-10-15 21:57:53 +0700

    Merapikan halaman profile

--- profil.php
+++ profil.php
@@ -5,7 +5,7 @@
     <title>Halaman Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- <script src="https://kit.fontawesome.com/f6dcf461c1.js" crossorigin="anonymous"></script> -->
+    <script src="https://kit.fontawesome.com/f6dcf461c1.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/profil.css">
   </head>
   <body class="bg-light">
@@ -14,6 +14,9 @@
       <h1>Profil</h1>
     </div>
     <div class="container bg-warning info">
+      <div class="exitbutton">
+        <button type="button" class="btn btn-outline-light active" name="exit"><i class="fas fa-door-open"></i></button>
+      </div>
     <div class="row">
       <div class="col-sm-3">
       <div class="container image">
