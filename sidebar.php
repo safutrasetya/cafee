87f3ade7225707d41ce964f9646315fa84459b5e
iwanva1s <81506951+iwanva1s@users.noreply.github.com>
2021-10-16 14:06:26 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <img src="/flexbox/img/img1.png" alt="" width="30" height="24">
+    <a class="navbar-brand" href="#" style ="margin-left: 5px">About Us</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      
+    </div>
+  </div>
+    <form class="d-flex">
+        <img src="/flexbox/img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px">
+        <img src="/flexbox/img/img1.png" alt="" width="30" height="24" style ="margin-right: 15px">
+    </form>
+</nav>
+<div class="second">
+    <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+        <header>Cafe</header>
+        <ul>
+            <li><a href=""><i class="far fa-address-card">Edit Akun</i></a></li>
+            <li><a href=""><i class="far fa-address-book">Daftar Akun</i></a></li>
+            <li><a href=""><i class="fas fa-clipboard-list">Daftar Makanan</i></a></li>
+            <li><a href=""><i class="fas fa-boxes"></i>Daftar Pesanan</a></li>
+            <li><a href=""><i class="fas fa-sign-out-alt"></i>Log Out</a></li>
+        </ul>
+    </div>
+    </div>
+    <section>
+
+    </section>
+    
+</body>
+</html>
