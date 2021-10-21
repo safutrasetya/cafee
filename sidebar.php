fc0c52331e6f654976754ccc75fa7251e8c14dc4
iwanva1s <yehezkielm13@gmail.com>
2021-10-21 15:00:51 +0700

    perbaikan nvabar2

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container-fluid">
-    <img src="/img/img1.png" alt="" width="30" height="24">
+    <img src="img/img1.png" alt="" width="30" height="24">
     <a class="navbar-brand" href="#" style ="margin-left: 5px">About Us</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -22,8 +22,8 @@
     </div>
   </div>
     <form class="d-flex">
-        <img src="/img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px">
-        <img src="/img/img1.png" alt="" width="30" height="24" style ="margin-right: 15px">
+        <img src="img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px">
+        <img src="img/img1.png" alt="" width="30" height="24" style ="margin-right: 15px">
     </form>
 </nav>
 <div class="second">
