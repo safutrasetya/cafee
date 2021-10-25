bc62c244d4fe3c51e4494a72e8dcba8fd6cc2192
iwanva1s <yehezkielm13@gmail.com>
2021-10-25 21:51:48 +0700

    perbaikan nav/side bar

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <title>Document</title>
@@ -12,7 +13,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container-fluid">
-    <img src="img/img1.png" alt="" width="30" height="24">
+    <img src="img/img1.png" alt="" width="35" height="35" style ="margin-right: 10px" class="rounded-circle">
     <a class="navbar-brand" href="#" style ="margin-left: 5px">About Us</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -22,8 +23,8 @@
     </div>
   </div>
     <form class="d-flex">
-        <img src="img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px">
-        <img src="img/img1.png" alt="" width="30" height="24" style ="margin-right: 15px">
+        <div style ="margin-top: 5px"><img src="img/img7.jpg" alt="" width="30" height="24" style ="margin-right: 15px"></div>
+        <img src="img/img1.png" alt="" width="35" height="35" style ="margin-right: 15px" class="rounded-circle">
     </form>
 </nav>
 <div class="second">
