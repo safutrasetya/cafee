380d56957bb5c7e689c02cf45a917690dafc9c97
iwanva1s <yehezkielm13@gmail.com>
2021-11-02 22:34:20 +0700

    masih blm selesai

---
+++ daftarpesanan.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- File css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+  <body>
+
+    <div class="container-dua">
+        <div class="layanan">
+            <h4>Layanan 1</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quisquam.</p>
+        </div>
+        <div class="layanan">
+            <h4>Layanan 2</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, quas.</p>
+        </div>
+        <div class="layanan">
+            <h4>Layanan 3</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, vitae!</p>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
