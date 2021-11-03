d0d7e89c7764a7940cd88ee4fcf03d4403eaf628
iwanva1s <yehezkielm13@gmail.com>
2021-11-03 19:28:56 +0700

    daftar pesanan

--- daftarpesanan.php
+++ daftarpesanan.php
@@ -5,31 +5,64 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- File css -->
+    <!-- File CSS -->
     <link rel="stylesheet" href="css/style.css">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title></title>
   </head>
   <body>
-
+    <?php include("temp_sidebar.php");?>
+      <div class="header">
+          <h3 class="text-center">Daftar Pesanan</h3>
+      </div>
     <div class="container-dua">
-        <div class="layanan">
-            <h4>Layanan 1</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quisquam.</p>
+        <div class="pesanan">
+            <h4>Pesanan 1</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
         </div>
-        <div class="layanan">
-            <h4>Layanan 2</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, quas.</p>
+        <div class="pesanan">
+            <h4>Pesanan 2</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
         </div>
-        <div class="layanan">
-            <h4>Layanan 3</h4>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, vitae!</p>
+        <div class="pesanan">
+            <h4>Pesanan 3</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
+        </div>
+        <div class="pesanan">
+            <h4>Pesanan 4</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
+        </div>
+        <div class="pesanan">
+            <h4>Pesanan 5</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
+        </div>
+        <div class="pesanan">
+            <h4>Pesanan 6</h4>
+            <p>Nasi Goreng (2)</p>
+            <p>Teh Manis (2)</p>
+            <h6>Meja 1</h6>
+            <button type="button" class="btn btn-success">Selesai</button>
         </div>
 
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
