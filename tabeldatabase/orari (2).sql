a39b9fd01831a526f9d4846756f38079b1d6fbbb
safutrasetya <safutrasetya@gmail.com>
2021-11-11 13:36:36 +0700

    tabel database baru

---
+++ tabeldatabase/orari (2).sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2021 at 07:35 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orari`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `No_Hp` varchar(15) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `level` varchar(1) NOT NULL,
+  `waktu_bergabung` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`id`, `username`, `nama`, `email`, `No_Hp`, `password`, `level`, `waktu_bergabung`) VALUES
+(1, 'admin', 'Admin Orari', 'adminOrari@gmail.com', '081279860111', 'admin', '1', '2021-11-01 12:18:28'),
+(2, 'Staff_1', 'Staff 1', 'staffOrari1@gmail.com', '083367893579', 'orari1201', '2', '2021-11-01 04:21:14'),
+(3, 'Staff_2', 'Staff 2', 'staffOrari2@gmail.com', '083659575277', 'orari1202', '2', '2021-11-01 04:21:24'),
+(4, 'Staff_3', 'Staff 3', 'staffOrari3@gmail.com', '088976123598', 'orari1203', '2', '2021-11-01 04:21:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meja`
+--
+
+CREATE TABLE `meja` (
+  `id_meja` int(11) NOT NULL,
+  `meja` int(2) NOT NULL,
+  `pass_meja` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `meja`
+--
+
+INSERT INTO `meja` (`id_meja`, `meja`, `pass_meja`) VALUES
+(1, 1, 'M0001'),
+(2, 2, 'M0002'),
+(3, 3, 'M0003'),
+(4, 4, 'M0004'),
+(5, 5, 'M0005'),
+(6, 6, 'M0006'),
+(7, 7, 'M0007'),
+(8, 8, 'M0008'),
+(9, 9, 'M0009'),
+(10, 10, 'M0010');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `nama_menu` varchar(30) NOT NULL,
+  `gambar` varchar(30) NOT NULL,
+  `info_menu` text NOT NULL,
+  `harga` int(10) NOT NULL,
+  `kategori` int(1) NOT NULL,
+  `ketersidiaan` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id_menu`, `nama_menu`, `gambar`, `info_menu`, `harga`, `kategori`, `ketersidiaan`) VALUES
+(1, 'Kentang Goreng', '', '', 10000, 3, 20),
+(2, 'Nugget ayam', '', '', 15000, 3, 30),
+(3, 'Pisang Goreng', '', '', 10000, 3, 40),
+(4, 'Roti bakar', '', '', 10000, 3, 50),
+(5, 'Teh Manis(P)', '', '', 7000, 2, 100),
+(6, 'Teh Manis(D)', '', '', 8000, 2, 100),
+(7, 'Kopi Hitam', '', '', 7000, 2, 100),
+(8, 'Kopi Susu', '', '', 10000, 2, 100),
+(9, 'Teh Susu (P)', '', '', 10000, 2, 100),
+(10, 'Teh Susu (D)', '', '', 12000, 2, 100),
+(11, 'Nasi Goreng', '', '', 15000, 1, 50),
+(12, 'Indomie (K)', '', '', 10000, 1, 50),
+(13, 'Nasi Ayam', '', '', 12000, 1, 50),
+(14, 'Indomie (G)', '', '', 10000, 1, 50),
+(15, 'Paket 1', '', '', 20000, 4, 50),
+(16, 'Paket 2', '', '', 30000, 4, 50),
+(17, 'Nasi Goreng Spesial', '', 'NASI GORENGNYA SPECIAL', 20000, 1, 1),
+(18, 'Nasi Ayam Bakar', '', 'NASINYA PAKE AYAM BAKAR', 20000, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id_pesanan` int(11) NOT NULL,
+  `id_meja` int(11) NOT NULL,
+  `id_keranjang` int(11) NOT NULL,
+  `nama_pesanan` varchar(30) NOT NULL,
+  `harga_pesanan` int(11) NOT NULL,
+  `waktu_pesanan` varchar(40) NOT NULL,
+  `status_pesanan` int(11) NOT NULL,
+  `banyak_psn` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat_pembelian`
+--
+
+CREATE TABLE `riwayat_pembelian` (
+  `id_pembelian` int(11) NOT NULL,
+  `id_meja` int(11) NOT NULL,
+  `id_keranjang` int(11) NOT NULL,
+  `total_pembayaran` int(10) NOT NULL,
+  `status_bayar` int(1) NOT NULL,
+  `waktu_pembayaran` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `riwayat_pembelian`
+--
+
+INSERT INTO `riwayat_pembelian` (`id_pembelian`, `id_meja`, `id_keranjang`, `total_pembayaran`, `status_bayar`, `waktu_pembayaran`) VALUES
+(1, 1, 2, 20000, 1, '2021-10-24'),
+(2, 2, 3, 25000, 2, '2021-10-25'),
+(3, 5, 8, 50000, 2, '2021-10-26'),
+(4, 4, 5, 100000, 1, '2021-10-27');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id_meja`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id_pesanan`);
+
+--
+-- Indexes for table `riwayat_pembelian`
+--
+ALTER TABLE `riwayat_pembelian`
+  ADD PRIMARY KEY (`id_pembelian`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `riwayat_pembelian`
+--
+ALTER TABLE `riwayat_pembelian`
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
