11ab6ce631df1bec4708a0ede24b57720746b1a9
immanuel1210 <immanuelsimarsoit@gmail.com>
2021-11-28 17:39:21 +0700

    28/11 Mini Update

--- daftarakun.php
+++ daftarakun.php
@@ -1,23 +1,23 @@
+<?php include("includes/koneksi.php");
+ include("includes/logincheck.php");
+ include("includes/admincheck.php");
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!--BOOSTRAP CSS AND CKEDITOR-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  	<script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/f6dcf461c1.js" crossorigin="anonymous"></script>
 
-    <!--END BOSTTSTRAP AND CKEDITOR-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
     <!--CSS KITA SENDIRI-->
     <link rel="stylesheet" href="css/akun.css">
     <link rel="stylesheet" href="css/cafee.css">
-    <!--end css kita sendiri-->
-    <title>cafee</title>
+    <title>Daftar Akun</title>
   </head>
   <body class="bg-light">
-    <?php include("includes/koneksi.php"); include("includes/logincheck.php");include("includes/admincheck.php");?>
     <?php include("temp_sidebar.php");?>
     <div class="jumbotron p-3 h-100" style="height: 750px;">
       <div class="jumbotron bg-light shadow-lg mx-auto p-5">
@@ -32,14 +32,11 @@
             <div class="col">
               <form action="" method="POST">
                 <div class="row">
-                  <!-- <div class=""> -->
-                    <!-- <a href="cariAkun.php"><button type="button" class="btn btn-outline-primary float-end my-2"><i class="bi bi-search"></i> Cari</button></a>
-                  </div> -->
                   <div class="col-sm-4" style="margin-left:320px;margin-top:7px;">
-                    <input type="text" name="keyword" class="form-control my-2" placeholder="Masukkan ID/Username/Nama..." autocomplete="off">
+                    <input type="text" name="keyword" id="keyword" class="form-control my-2" placeholder="Masukkan ID/Username/Nama..." autocomplete="off">
                   </div>
                   <div class="col-sm-2" style="margin-top:0px;padding:5px;">
-                    <input type="submit" name="cari" class="btn btn-primary my-2 form-control" value="Cari">
+                    <button type="submit" id="tombol-cari" name="cari" class="btn btn-primary my-2 form-control">Cari</button>
                   </div>
                   <div class="col text-end" style="margin-right:5px;">
                   <a href="tambahakun.php"><button type="button" class="btn btn-success my-2"><img src="img/tambahakun-icon2.png" style="height:30px; width:30px;"> Tambah akun</button></a>
@@ -53,8 +50,8 @@
           <div class="col-sm-2">
           </div>
           <div class="col-sm-10">
-            <div class="shadow">
-              <div class="body">
+            <div class="">
+              <div class="body" id="container">
                 <!-- <ul class="pagination pagination-sm justify-content-center">
                   <li class="page-item"><a class="page-link" href="#">Previous</a></li>
                   <li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -87,13 +84,7 @@
                         $jumlahAkun = mysqli_num_rows($akun);
                         $totalHalaman = ceil($jumlahAkun / $perHalaman);
 
-                        if(!isset($_POST['cari'])){
-                          $akuns = mysqli_query($koneksi,"SELECT * FROM akun LIMIT $halamanAwal, $perHalaman");
-                        }elseif(isset($_POST['cari'])){
-                          $cari = $_POST['keyword'];
-                          $akuns = mysqli_query($koneksi,"SELECT * FROM akun WHERE nama LIKE '%$cari%' OR
-                                   username LIKE '%$cari%' LIMIT $halamanAwal,$perHalaman");
-                        }
+                        $akuns = mysqli_query($koneksi,"SELECT * FROM akun LIMIT $halamanAwal, $perHalaman");
                         while($d = mysqli_fetch_assoc($akuns)){
                      ?>
                     <tr>
@@ -180,4 +171,5 @@
 
     </script> -->
   </body>
+  <script src="js/searchakun.js"></script>
 </html>
