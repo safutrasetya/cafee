8dbb15325931fd7a882805720cd40d1efe5f937a
iwanva1s <yehezkielm13@gmail.com>
2021-11-29 09:36:46 +0700

    mini change

--- daftarpesanan.php
+++ daftarpesanan.php
@@ -50,6 +50,15 @@ require 'functions.php';
     margin: 20px;
 
 }
+.pesanan-selesai{
+  width: 200px;
+    padding: 20px;
+    box-sizing: border-box;
+    background-color: #dc3545;
+    margin-top: 20px;
+    margin: 20px;
+
+}
     </style>
   </head>
   <body>
@@ -106,6 +115,61 @@ while ($d = mysqli_fetch_array($data)){
         </div>
 
     </div>
+
+
+        <div class="header">
+            <h3 class="text-center">Daftar Pesanan (Selesai)</h3>
+        </div>
+      <div class="container-dua border border-white border-3">
+
+    <?php
+
+    // foreach($id_trans as $id_tran):
+
+    //   $pesanan = mysql_query("SELECT nama_pesanan, banyak_psn,id_meja FROM pesanan WHERE id_transaksi =  20");
+
+    $data = mysqli_query($connect, "SELECT  * FROM riwayat_pembelian WHERE status_pesanan=1  ");
+    while ($d = mysqli_fetch_array($data)){
+
+    ?>
+
+        <div class="pesanan-selesai">
+            <h4 style="text-align: center" >
+
+            <span class="border-bottom border-white border-3">Pesanan <?= $d['id_transaksi']?></span>
+            </h4>
+              <!-- <div class="ex1"> -->
+           <!-- <p>id Transaksi : <?=$d['id_transaksi'] ?></p> -->
+           <?php $id_kita=$d['id_transaksi'] ?>
+
+
+
+          <!-- </div>   -->
+
+          <h5>Nama Pesanan :</h5>
+        <?php
+        $data_pesanan = mysqli_query($connect, "SELECT * from pesanan where id_transaksi = $id_kita AND status_pesanan=0");
+        while ($dp = mysqli_fetch_array($data_pesanan)){
+            ?>
+          <p><?= $dp['nama_pesanan'] ?> (<?= $dp['banyak_psn']?>)</p>
+
+          <?php } ?>
+          <div class="d-grid gap-2">
+                <a href="funct_psn_kembali.php?id_kita=<?= $id_kita?>" class="btn btn-dark"> Kembalikan</a>
+          </div>
+
+
+          </div>
+
+          <?php
+
+              }
+         ?>
+          </div>
+
+      </div>
+
+
 
 
 
