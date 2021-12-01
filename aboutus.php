9a6e593c2723eedc3e0b98ae3fd9cde15cb2ae1c
safutrasetya <safutrasetya@gmail.com>
2021-12-01 22:44:21 +0700

    FINAL FINAL

--- aboutus.php
+++ aboutus.php
@@ -22,5 +22,7 @@
       <p style="font: arial">Kenyamanan pelanggang adalah yang utama bagi kami.</p><br></div>
 	</div>
 <script src="js/aboutus.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </body>
 </html>
